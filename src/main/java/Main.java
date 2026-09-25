e19db8e19912606ea0310517b30910f6ebc8462c
Vincent de Jong <a.v.de.jong@st.hanze.nl>
2026-09-25 13:42:58 +0200

    eerste testversie voor commandline met picocli

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,37 @@
-public class Main {
-}
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Help;
 
-hoi
+@Command(name = "Java-neanderthaler-app",
+        description = "Test description want ik heb geen zin in een echte te bedenken nu!",
+        mixinStandardHelpOptions = true)
+
+public class Main implements Runnable {
+
+    @Option(
+            names = {"--human"},
+            description = "Path voor VCF file van jouw gekozen mens",
+            required = true
+    )
+    private String humanvcf;
+
+    @Option(
+            names = {"--neanderthaler"},
+            description = "Path voor VCF file van de Neanderthaler (voorbeeld path)",
+            required = true
+    )
+    private String neanderthalervcf;
+
+    @Override
+    public void run() {
+        System.out.println("Mens VCF: " + humanvcf);
+        System.out.println("Neanderthaler VCF: " + neanderthalervcf);
+    }
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new Main()).execute(args);
+        System.exit(exitCode);
+    }
+}
